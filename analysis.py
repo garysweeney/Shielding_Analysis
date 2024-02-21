0f5cdf6fad56590f56e2b15c94a8a64b41a1a2a9
Gary Sweeney <59615188+garysweeney@users.noreply.github.com>
2024-02-21 17:15:26 -0500

    Commit water shielding results

--- analysis.py
+++ analysis.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import rc
-from scipy.optimize import curve_fit
 
 rc('font', **{'family': 'serif', 'serif': ['computer modern']})
 rc('text', usetex=True)
@@ -11,21 +10,22 @@ def read_neutrons(file):
     energy=data[:,2]
     return energy
 
-no_shield = len(read_neutrons("no_shield.txt"))
+def integrate_spectrum(file):
+    flux = read_neutrons(file)
+    return(len(flux))
+
+no_shield = integrate_spectrum("no_shield.txt")
 
 # water
-water_5cm = len(read_neutrons("water/water_5cm.txt"))
-water_10cm = len(read_neutrons("water/water_10cm.txt"))
-water_15cm = len(read_neutrons("water/water_15cm.txt"))
-water_20cm = len(read_neutrons("water/water_20cm.txt"))
-water_25cm = len(read_neutrons("water/water_25cm.txt"))
-water_30cm = len(read_neutrons("water/water_30cm.txt"))
+water = [no_shield]
+for i in range(1,12):
+    water.append(integrate_spectrum("water/water_{}cm.txt".format(i*5)))
 
-depths = [0,5,10,15,20,25,30]
-counts = [no_shield, water_5cm, water_10cm, water_15cm, water_20cm, water_25cm, water_30cm]
+depth = np.linspace(0, 150, 31)
 
 plt.figure()
-plt.scatter(depths, counts)
-#plt.yscale("log")
-plt.xlim(-0.1,150)
+plt.scatter(depth[:len(water)], water)
+plt.yscale("log")
+plt.xscale("log")
+plt.xlim(-0.1,100)
 plt.show()
