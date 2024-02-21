3ecc4e73b6a5380f473b8c6223e094e666fe658b
Gary Sweeney <59615188+garysweeney@users.noreply.github.com>
2024-02-21 01:57:40 -0500

    Commit water shielding results

--- analysis.py
+++ analysis.py
@@ -19,12 +19,13 @@ water_10cm = len(read_neutrons("water/water_10cm.txt"))
 water_15cm = len(read_neutrons("water/water_15cm.txt"))
 water_20cm = len(read_neutrons("water/water_20cm.txt"))
 water_25cm = len(read_neutrons("water/water_25cm.txt"))
+water_30cm = len(read_neutrons("water/water_30cm.txt"))
 
-depths = [0,5,10,15,20,25]
-counts = [no_shield, water_5cm, water_10cm, water_15cm, water_20cm, water_25cm]
+depths = [0,5,10,15,20,25,30]
+counts = [no_shield, water_5cm, water_10cm, water_15cm, water_20cm, water_25cm, water_30cm]
 
 plt.figure()
 plt.scatter(depths, counts)
-plt.yscale("log")
+#plt.yscale("log")
 plt.xlim(-0.1,150)
 plt.show()
