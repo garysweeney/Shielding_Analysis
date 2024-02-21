a9ebb4168563da3ed1c716131688f22ae124cab2
Gary Sweeney <59615188+garysweeney@users.noreply.github.com>
2024-02-21 00:50:01 -0500

    Commit water shielding tests

--- analysis.py
+++ analysis.py
@@ -11,20 +11,20 @@ def read_neutrons(file):
     energy=data[:,2]
     return energy
 
-def compute_area(radius):
-    return 4. * np.pi * radius**2
-
-no_shield = len(read_neutrons("no_shield.txt")) / compute_area(0.001)
+no_shield = len(read_neutrons("no_shield.txt"))
 
 # water
-water_5cm = len(read_neutrons("water/water_5cm.txt")) / compute_area(0.05)
-water_10cm = len(read_neutrons("water/water_10cm.txt")) / compute_area(0.10)
-water_15cm = len(read_neutrons("water/water_15cm.txt")) / compute_area(0.15)
+water_5cm = len(read_neutrons("water/water_5cm.txt"))
+water_10cm = len(read_neutrons("water/water_10cm.txt"))
+water_15cm = len(read_neutrons("water/water_15cm.txt"))
+water_20cm = len(read_neutrons("water/water_20cm.txt"))
+water_25cm = len(read_neutrons("water/water_25cm.txt"))
 
-depths = [0,5,10,15]
-counts = [no_shield, water_5cm, water_10cm, water_15cm]
+depths = [0,5,10,15,20,25]
+counts = [no_shield, water_5cm, water_10cm, water_15cm, water_20cm, water_25cm]
 
 plt.figure()
 plt.scatter(depths, counts)
 plt.yscale("log")
+plt.xlim(-0.1,150)
 plt.show()
