dd9b74ddf96dc105708684c88f20ffb350fb8aff
Gary Sweeney <59615188+garysweeney@users.noreply.github.com>
2024-02-22 15:53:55 -0500

    Commit borated water results

--- analysis.py
+++ analysis.py
@@ -1,14 +1,15 @@
-
-"""
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import rc
 from scipy.optimize import curve_fit
+from computed_livetime import compute_livetime
 
 rc('font', **{'family': 'serif', 'serif': ['computer modern']})
 rc('text', usetex=True)
-"""
-"""
+
+nEvents = 1e6
+livetime = compute_livetime(nEvents)
+
 def read_neutrons(file):
     data = np.genfromtxt(file)
     energy=data[:,2]
@@ -20,53 +21,35 @@ def integrate_spectrum(file):
 
 no_shield = integrate_spectrum("no_shield.txt")
 
-
 # water
 water = [no_shield]
+borated_water = [no_shield]
+
 for i in range(1,21):
     water.append(integrate_spectrum("water/water_{}cm.txt".format(i*5)))
 
+for i in range(1,20):
+    borated_water.append(integrate_spectrum("borated_water/borated_water_{}cm.txt".format(i*5)))
+borated_water.append(1)
+
 depth = np.linspace(0, 100, 21)
 
-print (depth)
-print(water)
+# Compute normalization to  n/m2/yr
+# Detector sphere surface area:
+surf_area = 4 * np.pi * 1.1**2 #m2
+
+water_normalized = [i / (surf_area * livetime) for i in water]
+borated_water_normalized = [i / (surf_area * livetime) for i in borated_water]
 
 plt.figure()
-plt.scatter(depth[:len(water)], water)
-#plt.yscale("log")
-#plt.xscale("log")
-plt.xlim(-0.1,100)
-plt.show()
-"""
-
-import numpy as np
-from scipy.optimize import curve_fit
-import matplotlib.pyplot as plt
-
-# Given data
-x_data = np.array([0., 5., 10., 15., 20., 25., 30., 35., 40., 45., 50., 55., 60., 65., 70., 75., 80., 85., 90., 95., 100.])
-y_data = np.array([1003422, 902537, 562730, 302595, 150637, 73779, 35439, 17201, 8380, 4200, 2040, 1103, 574, 285, 190, 92, 36, 31, 11, 9, 4])
-
-# Define the power-law function
-def power_law(x, a, b, c, d, e, f):
-    return np.cosh(b * np.sqrt(x)) * (np.exp(-c * x) + np.exp(-d * x))
-
-# Fit the curve using least squares
-popt, pcov = curve_fit(power_law, x_data, y_data)
-
-# Generate y values for the fitted curve
-y_fit = power_law(x_data, *popt)
-
-# Plot the original data and the fitted curve
-plt.scatter(x_data, y_data, label='Original Data')
-plt.plot(x_data, y_fit, 'r-', label='Fitted Curve')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('Curve Fitting with Logarithmic Scale on y-axis (Power Law)')
-plt.legend()
+plt.xticks(fontsize=12)
+plt.yticks(fontsize=12)
+plt.xlabel("Depth (cm)", fontsize=12)
+plt.ylabel(r"Observed Flux (n/m$^2$/yr)", fontsize=12)
+plt.scatter(depth, water_normalized, label = "Water", color='blue')
+plt.scatter(depth, borated_water_normalized, label = "Borated Water", color='red')
+plt.plot(depth,water_normalized,color="blue")
+plt.plot(depth,borated_water_normalized,color="red")
 plt.yscale("log")
-plt.grid(True)
+plt.xscale("log")
 plt.show()
-
-# Print the optimized parameters
-print("Optimized Parameters:", popt)
---
+++ computed_livetime.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+def compute_livetime(nEvents):
+
+    # Load the norite rock data
+    file = "sim_data.dat"
+    data = np.genfromtxt(file, dtype=str)
+
+    # Obtain production rates [n/g/s/ppb]
+    production_rate = (data[2:,1])
+    production_rate = [float(i) for i in production_rate]
+
+    # Obtain activity [ppb]
+    activity = (data[2:,2])
+    activity = [float(i) for i in activity]
+
+    # Compute mass of norite sphere
+    volume = ((4 * np.pi) / 3) * (340.**3 - 270.**3) # 4pi/3 r^3 [cm3]
+    density = 2.88 # [g/cm3]
+    mass = volume * density #[g]
+
+    # Apply U-235 abundance factor
+    livetime = (nEvents / (production_rate[3] * activity[3] * mass)) * 0.007
+    for i in range(3):
+        livetime += (nEvents / (production_rate[i] * activity[i] * mass))
+
+    # Convert to years
+    livetime /= (365 * 24 * 3600)
+
+    return livetime
